38c211afeca22cddd74a99b7eff50a91eb7d7e75
Atul Vishwakarma <atulvishw240@gmail.com>
2026-02-16 18:21:37 +0530

    Add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, :email, :password, presence: true
+  validates :username, length: { in: 5..15 }
+  validates :password, length: { in: 5..12 }
+  validates :username, :email, uniqueness: true
 end
