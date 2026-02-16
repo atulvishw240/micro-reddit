7dc231104ae62ba3437b6589fd2dabf291536769
Atul Vishwakarma <atulvishw240@gmail.com>
2026-02-16 18:47:14 +0530

    Migrate db to reflect comment and add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
+  validates :body, :user_id, :post_id, presence: true
+
   belongs_to :user, :post
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_16_125505) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_16_131248) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.integer "post_id"
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
